5744e3f1be7c3b33db0476f24072e710188eec6f
MariaDelCarmenLuque <maritcarmn@gmail.com>
2024-09-27 07:14:35 -0500

    feat: add prep 4 task

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,76 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int number;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        do
+        {
+            Console.Write("Enter number: ");
+            number = int.Parse(Console.ReadLine());
+
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+
+        } while (number != 0);
+
+        // Core Requeriments
+
+        // 1. Sum of the numbers in the list
+        int sum = 0;
+        foreach (int num in numbers)
+        {
+            sum += num;
+        }
+        Console.WriteLine($"The sum is: {sum}");
+
+        // 2. Average of the numbers in the list
+        if (numbers.Count > 0)
+        {
+            double average = (double)sum / numbers.Count;
+            Console.WriteLine($"The average is: {average}");
+        }
+
+        // 3. Maximum number in the list
+        int maxNumber = numbers[0];
+        foreach (int num in numbers)
+        {
+            if (num > maxNumber)
+            {
+                maxNumber = num;
+            }
+        }
+        Console.WriteLine($"The largest number is: {maxNumber}");
+
+        // 4. Minimum positive number of the list
+        int smallestPositive = -1;
+        foreach (int num in numbers)
+        {
+            if (num > 0 && (smallestPositive == -1 || num < smallestPositive))
+            {
+                smallestPositive = num;
+            }
+        }
+
+        if (smallestPositive > 0)
+        {
+            Console.WriteLine($"The smallest positive number is: {smallestPositive}");
+        }
+        else
+        {
+            Console.WriteLine("There are no positive numbers.");
+        }
+
+        // 5. Sort the numbers of the list
+        numbers.Sort();
+        Console.WriteLine("The sorted list is:");
+        foreach (int num in numbers)
+        {
+            Console.WriteLine(num);
+        }
     }
 }
