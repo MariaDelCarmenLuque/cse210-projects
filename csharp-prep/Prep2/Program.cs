74d02a6dbb59fbcfd0d30794eda18ac64fa94010
MariaDelCarmenLuque <maritcarmn@gmail.com>
2024-09-26 23:27:33 -0500

    feat: finished prep 2 task

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string grade = Console.ReadLine();
+
+        int gradePercentage = int.Parse(grade);
+        string letterGrade;
+
+        if(gradePercentage>= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (gradePercentage >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (gradePercentage >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (gradePercentage >= 60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letterGrade}");
+        
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("Congratulations.  You passed the course.");
+        }
+        else
+        {
+            Console.WriteLine("Don't be discouraged! Keep trying, and you'll do better next time.");
+        }
+
     }
 }
