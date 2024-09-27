f90a0d679a240c71fa0604cb8a3cc1870e581d30
MariaDelCarmenLuque <maritcarmn@gmail.com>
2024-09-27 06:53:33 -0500

    feat: add prep 3 task

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string playAgain = "yes";
+        while(playAgain.ToLower() == "yes")
+        { 
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 51);
+            int guessNumber = 0;
+        
+
+            while(guessNumber != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                string guess=Console.ReadLine();
+                guessNumber = int.Parse(guess);
+
+                if (magicNumber> guessNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (magicNumber<guessNumber) 
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guesses it!");
+                }
+            }
+
+            Console.Write("Do you want to play again? (yes/no): ");
+            playAgain = Console.ReadLine();
+        }
     }
 }
