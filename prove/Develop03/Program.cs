ceb6338e944caef5213b42f4ea503bb6c5b9ab40
MariaDelCarmenLuque <maritcarmn@gmail.com>
2024-10-27 23:46:48 -0500

    Develop03: update program cs

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        Reference reference = new Reference("Proverbs", 3, 5, 6);
+        Scripture scripture = new Scripture(reference, "Trust in the Lord with all your heart and lean not unto thine own understanding; in all thy ways ackowledge him, and he shall direct thy paths.");
+
+        while (!scripture.IsCompletelyHidden())
+        {
+            Console.Clear();
+            Console.WriteLine(scripture.GetDisplayText());
+            Console.WriteLine("\nPress Enter to hide words or type 'quit' to exit.");
+            string response = Console.ReadLine();
+
+            if (response.ToLower() == "quit")
+            {
+                break;
+            }
+
+            scripture.HideRandomWords(3);
+        }
+
+        Console.Clear();
+        Console.WriteLine("All words are hidden!");
     }
 }
