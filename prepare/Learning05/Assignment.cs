f9fc4d3ad9e189c41afbef8f02431dd879586df3
MariaDelCarmenLuque <maritcarmn@gmail.com>
2024-10-28 21:55:00 -0500

    Learning05: add assignment and math-assignment classes

---
+++ prepare/Learning05/Assignment.cs
@@ -0,0 +1,22 @@
+using System.Reflection.Metadata.Ecma335;
+
+public class Assignment {
+    private string _studentName = "";
+    private string _topic ="";
+
+    public Assignment(string studentName, string topic)
+    {
+         _studentName = studentName;
+         _topic =  topic;
+    }
+
+    public void SetTopic(string topic)
+    {
+        _topic=topic;
+    }
+
+    public string GetSummary()
+    {
+        return $"{_studentName} - {_topic}";
+    }
+}
--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+       Assignment assignment1 = new Assignment("Samuel Bennett", "Multiplication");
+    //    Console.WriteLine(assignment1.GetSummary());
+
+        MathAssignment mathAssignment = new MathAssignment("Samuel Bennett","Multiplication", "7.3", "8-19");
+        Console.WriteLine(mathAssignment.GetSummary()); 
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
     }
 }
